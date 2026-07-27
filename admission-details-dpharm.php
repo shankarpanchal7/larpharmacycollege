07903c3a525642c1abd22d94a01bfe98a2054086
shankar <shankarpanchal7@sdlccorp.com>
2026-07-27 17:23:12 +0530

    header component fixed

---
+++ admission-details-dpharm.php
@@ -0,0 +1,52 @@
+<?php
+include_once("components/header_t.php");
+echo '<title>Diploma of Pharmacy</title>';
+include_once("components/header_b.php"); ?>
+    <style>
+        .admission-details-dpharm-text {
+            text-align: justify;
+        }
+    </style>
+    
+
+    <section class="section-silent-features" id="js--active-sticky">
+    <div class="container">
+    <h2>Diploma of Pharmacy</h2>
+
+    </div>
+    <div class="container">
+        <div>
+            <div class="silent-features-block">
+            <h5><b><i class="icon ion-md-school"></i> D. Pharmacy</b></h5><br>
+
+            <div>
+            <h3>Duration – 2 years</h3>
+
+    
+                <p class="training-career-guidance-cell-text">
+                Affiliation- Maharashtra state board of Technical education, Mumbai.
+Intake- 60
+    </p>
+    <h3>Eligibility criteria </h3>
+    <li> The candidate should be an Indian Citizen.</li>
+<li>  Passed HSC or its equivalent examination with Physics and Chemistry as compulsory subject along with one of the mathematics or Biology.</li><br><br>
+
+
+                </div>
+        </div>
+
+    </section>
+
+   
+
+
+   
+
+  
+
+
+    
+   
+<?php
+  include_once('components/footer.php');
+?>
--- components/header_t.php
+++ components/header_t.php
@@ -1,45 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Loknete Appasaheb Rajale College of Pharmacy offers Diploma in Pharmacy (D. Pharm.) and Bachelor of Pharmacy (B. Pharm.) courses. Approved by DTE Mumbai, PCI New Delhi, Affiliated to DBATU and MSBTE."/>
-    <meta name="keywords" content="larcop,pharmacy,pharmacy course,b pharmacy college,d pharmacy college,pharmacy colleges adinathnagar,pharmacy colleges pathardi,pharmacy colleges ahilyanagar"/>
-        <link rel="canonical" href="https://pharmacy.dprss.in" />
-        <meta property="og:locale" content="en-IN" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Loknete Appasaheb Rajale College of Pharmacy" />
-        <meta property="og:description" content="Loknete Appasaheb Rajale College of Pharmacy - D. Pharm & B. Pharm Courses" />
-        <meta property="og:url" content="https://pharmacy.dprss.in" />
-        <meta property="og:site_name" content="Loknete Appasaheb Rajale College of Pharmacy" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:description" content="Loknete Appasaheb Rajale College of Pharmacy - Adinathnagar, Pathardi, Ahilyanagar" />
-        <meta name="twitter:title" content="Loknete Appasaheb Rajale College of Pharmacy" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description"
+    content="Loknete Appasaheb Rajale College of Pharmacy offers Diploma in Pharmacy (D. Pharm.) and Bachelor of Pharmacy (B. Pharm.) courses. Approved by DTE Mumbai, PCI New Delhi, Affiliated to DBATU and MSBTE." />
+  <meta name="keywords"
+    content="larcop,pharmacy,pharmacy course,b pharmacy college,d pharmacy college,pharmacy colleges adinathnagar,pharmacy colleges pathardi,pharmacy colleges ahilyanagar" />
+  <link rel="canonical" href="https://pharmacy.dprss.in" />
+  <meta property="og:locale" content="en-IN" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Loknete Appasaheb Rajale College of Pharmacy" />
+  <meta property="og:description"
+    content="Loknete Appasaheb Rajale College of Pharmacy - D. Pharm & B. Pharm Courses" />
+  <meta property="og:url" content="https://pharmacy.dprss.in" />
+  <meta property="og:site_name" content="Loknete Appasaheb Rajale College of Pharmacy" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:description"
+    content="Loknete Appasaheb Rajale College of Pharmacy - Adinathnagar, Pathardi, Ahilyanagar" />
+  <meta name="twitter:title" content="Loknete Appasaheb Rajale College of Pharmacy" />
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="vendors/css/normalize.css">
-    <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
-    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS -->
+  <link rel="stylesheet" href="vendors/css/normalize.css">
+  <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+  <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <!-- Web Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700&display=swap" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,700&display=swap" rel="stylesheet">
-    <!-- Favicon Icons -->
-    
-    <link rel="apple-touch-icon" sizes="76x76" href="">
-    <link rel="icon" type="image/png" sizes="32x32" href="">
-    <link rel="icon" type="image/png" sizes="16x16" href="">
-    <link rel="manifest" href="">
-    <link rel="mask-icon" href="" color="#44000d">
-    <link rel="shortcut icon" href="">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="">
-    <meta name="theme-color" content="#ffffff">
-    <!-- <link rel="stylesheet" href="css/style2.css"> -->
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/section.css">
-    
-    <link rel="stylesheet" href="resources/css/queries.css">
+  <!-- Web Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,700&display=swap" rel="stylesheet">
+  <!-- Favicon Icons -->
+
+  <link rel="apple-touch-icon" sizes="76x76" href="">
+  <link rel="icon" type="image/png" sizes="32x32" href="">
+  <link rel="icon" type="image/png" sizes="16x16" href="">
+  <link rel="manifest" href="">
+  <link rel="mask-icon" href="" color="#44000d">
+  <link rel="shortcut icon" href="">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-config" content="">
+  <meta name="theme-color" content="#ffffff">
+  <!-- <link rel="stylesheet" href="css/style2.css"> -->
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/section.css">
+
+  <link rel="stylesheet" href="resources/css/queries.css">
